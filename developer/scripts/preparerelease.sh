5e8f9c1f14f342d8defbeb735f08da9ccf276dcf
mjk <mjk@afb8ca49-fd4f-46c6-8201-93275541bcc6>
2006-08-08 07:43:15 +0000

    license statement added

--- developer/scripts/preparerelease.sh
+++ developer/scripts/preparerelease.sh
@@ -61,6 +61,10 @@ steptwo() {
 KOMA-Script $version
 Copyright (c) Markus Kohm <komascript(at)gmx.info>, 1994-2006
 
+This material is subject to the LaTeX Project Public License. See
+lppl.txt (english) or lppl-de.txt (german) for the details of that
+license.
+
 --------------------------------------------------------------------
 EOF
     [ "$versionpostfix" = "$version" ] || cat >> README <<EOF
