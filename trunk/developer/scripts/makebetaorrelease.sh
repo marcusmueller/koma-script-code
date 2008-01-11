642eee045d95dcafc0715776d145ad06f5a43966
mjk <mjk@afb8ca49-fd4f-46c6-8201-93275541bcc6>
2008-01-11 10:11:59 +0000

    *** empty log message ***

---
+++ trunk/developer/scripts/makebetaorrelease.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+# Copyright (c) Markus Kohm, 2008
+#
+
+if [ $# -lt 2 ]; then
+    echo "Usage: makebeta.sh <date> <version> [...]" >&2
+    echo "Example: makebeta.sh 2007/05/12 2.97c BETA" >&2
+    exit 1
+fi
+
+datestr=$1
+versionstr=$2
+shift 2
+additional="$*"
+while [ "$additional" != "${additional% }" ]; do
+    additional=${additional% }
+done
+while [ "$additional" != "${additional# }" ]; do
+    additional=${additional# }
+done
+[ -z "$additional" ] || additional=" $additional"
+
+if svn status | grep '^M'; then
+    echo "There are still modified files!" >&2
+    exit 1
+fi
+
+if ! grep 'scr@v@'"${versionstr}" scrkcomp.dtx; then
+    cp -f scrkcomp.dtx scrkcomp.dtx~
+    sed 's!\(\\begin{macro}{\\scr@v@last}\)!\\begin{macro}{\\scr@v@'"${versionstr}"'}\n%   \\changes{v'"${versionstr}"'}{'"${datestr}"'}{Neues Macro}\n% \1!' scrkcomp.dtx > scrkcomp.dtx.tmp
+    sed '/^\\@namedef{scr@v@last}{[0-9]*} *$/{N;s!^\\@namedef{scr@v@last}{\([0-9]\)*} *\n%    \\end{macrocode}!\\@namedef{scr@v@'"${versionstr}"'}{\1}\n\0\n% \\end{macro}!}' scrkcomp.dtx.tmp > scrkcomp.dtx
+    curr_checksum=`head -1 scrkcomp.dtx | grep -o '[0-9]\+'`
+    new_checksum=$(( ${curr_checksum} + 1 ))
+    sed 's/^\(% *\\CheckSum{\).*\(}.*\)$/\1'${new_checksum}'\2/' scrkcomp.dtx > scrkcomp.dtx.tmp
+    mv scrkcomp.dtx.tmp scrkcomp.dtx
+    if latex -interaction=batchmode scrkcomp.dtx; then
+	rm scrkcomp.dtx~
+    else
+	mv -f scrkcomp.dtx scrkcomp.dtx.tmp
+	mv -f scrkcomp.dtx~ scrkcomp.dtx
+	echo "Error changing scrkcomp.dtx (see scrkcomp.dtx.tmp)." >&2
+	exit 1
+    fi
+fi
+
+sed 's!\(\\@CheckKOMAScriptVersion{\)[^}]*!\1'"${datestr} v${versionstr}${additional} KOMA-Script!" scrkvers.dtx > scrkvers.dtx.new
+mv -f scrkvers.dtx.new scrkvers.dtx
+
+echo "Do next:"
+echo "  make"
+echo "  svn ci scrkvers.dtx scrkcomp.dtx -m 'prepared for ${versionstr}${additional}'"
+echo "  developer/scripts/addrelease.sh '${datestr}' '${versionstr}' \"$@\""
+echo "(NOTE: Don't execute addrelease.sh on BETA versions.)"
+echo "or:"
+echo "  rm scrkvers.dtx scrkcomp.dtx;svn up scrkvers.dtx scrkcomp.dtx"
