227377c09c1d37851ccb748df988e8394e7c6c01
kohm <25980208+komascript@users.noreply.github.com>
2020-04-20 09:50:39 +0000

    new make rules for tagging and uploading

--- obsolete/build.lua
+++ obsolete/build.lua
@@ -20,7 +20,39 @@ bundle = "koma-script-obsolete"
 modules = { "scrlettr-obsolete", "scrpage-obsolete", "scrpage2-obsolete" }
 textfiles = { "README" }
 
+-- Tagging
+
+tagfiles = { "build.lua", "README" }
+
+function update_tag(file,contents,tagname,tagdate)
+   if string.match(file, "build.lua") then
+      return string.gsub(contents,
+			 '(\n%s*version%s*=%s*)"[^"]+"',
+			 '%1' .. tagname)
+   elseif string.match(file, "README") then
+      return string.gsub(contents,
+			 '(\nVersion:%s+)[^\n]*\n',
+			 '%1' .. tagname .. '\n', 1)
+   end
+   return contents
+end
+
+-- CTAN
+
 packtdszip = false
+uploadconfig = {
+   pkg         = "koma-script-obsolete",
+   version     = "2020-04-19",
+   author      = "Markus Kohm",
+   license     = "lppl1.3c",
+   summary     = "Deprecated packages from KOMA-Script",
+   ctanPath    = "obsolete/macros/latex/contrib/koma-script-obsolete",
+   description = "<p>The bundle provides copies of old versions of packages in the current koma-script bundle.</p><p>Packages in the obsolete 'distribution' are scrlttr2, scrpage, and scrpage2; they should no tbe used in new docments, but are preserved for use in existing documents.</p>",
+   home        = "https://komascript.de",
+   repository  = "http://svn.code.sf.net/p/koma-script/code/trunk/obsolete/",
+   topic       = "obsolete",
+   update      = true      
+}
 
 -- Find and run build system
 
